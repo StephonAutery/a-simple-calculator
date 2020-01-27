969dc49ff818a047eeb711343280aa6d78a9c7d5
StephonAutery <stephon@stephonautery.com>
2020-01-27 15:00:58 -0800

    initialing the repository

---
+++ index.html
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Stephon Autery - a simple calculator</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="icon" type="image/png" href="favicon.png">
+</head>
+<body>
+
+    <div id="titleDiv"><h2>a simple calculator</h2></div>
+
+    <div id="displayDiv">
+        <p id="display"></p>
+    </div>
+
+    <div id="opKeys">
+        <button class="function" id="key10">+</button>
+        <button class="function" id="key11">-</button>
+        <button class="function" id="key12">*</button>
+        <button class="function" id="key13">/</button>
+    </div>  
+
+    <div id="numKeys">
+        <p class="pKeys">
+            <button class="keys" id="key1">1</button>
+            <button class="keys" id="key2">2</button>
+            <button class="keys" id="key3">3</button>
+        </p>
+        <p class="pKeys">
+            <button class="keys" id="key4">4</button>
+            <button class="keys" id="key5">5</button>
+            <button class="keys" id="key6">6</button>
+        </p>
+        <p class="pKeys">
+            <button class="keys" id="key7">7</button>
+            <button class="keys" id="key8">8</button>
+            <button class="keys" id="key9">9</button>
+        </p>
+    </div>
+
+    <div id="funKeys">
+        <button class="function" id="clear">C</button>
+        <button class="function" id="process">=</button>
+    </div>
+
+    <div><p id="footer">copyright 2020 - stephon autery</p></div>
+
+    <script src="javaScript.js"></script>
+</body>
+</html>
---
+++ javaScript.js
@@ -0,0 +1,103 @@
+
+// build the numbers
+function makeNum(event){
+    if (gotTotal === true){
+        displayClear();
+    }  
+    display.appendChild(document.createTextNode(event.target.textContent));
+    var number = event.target.textContent;
+    if (gotFirst){
+        secondNumb = parseInt(secondNumb + number);
+        gotSecond = true;
+    } else {
+        firstNumb = parseInt(firstNumb + number);
+    }
+}
+
+// clear the display and reset all values
+function displayClear(){
+    var keyTarget = document.createTextNode("");
+    display.textContent = "";
+    firstNumb = "";
+    secondNumb = "";
+    gotFirst = false;
+    gotTotal = false;
+    gotSecond = false;
+    gotOperator = false;
+}
+
+// get the operator
+function getOperator(event){
+    console.log(gotFirst);
+    console.log(gotOperator);
+    if (gotOperator === false && firstNumb != ""){
+        operator = event.target.textContent;
+        display.appendChild(document.createTextNode(" "));
+        display.appendChild(document.createTextNode(event.target.textContent));
+        display.appendChild(document.createTextNode(" "));
+        gotFirst = true;
+        gotOperator = true;
+    }
+}
+
+// do the math!
+function getVal(event){
+    if (gotSecond === true && gotFirst === true && gotTotal === false){
+        var getOp = document.createTextNode(event.target.textContent);
+        getOp = event.target.textContent;
+        var doIt = 0;
+        switch (operator){
+            case "*":
+                var doIt = firstNumb * secondNumb;
+                break;
+            case "-":
+                var doIt = firstNumb - secondNumb;
+                break;
+            case "/":
+                var doIt = firstNumb / secondNumb;
+                break;
+            case "+":
+                var doIt = firstNumb + secondNumb;
+        }
+        display.appendChild(document.createTextNode("  =  "));    
+        display.appendChild(document.createTextNode(doIt));
+        gotTotal = true;
+    }
+}
+
+// variable declarations and settings
+var getNum = "";
+var getKey = 0;
+var firstNumb  = "";
+var secondNumb = "";
+var gotFirst = false;
+var gotSecond = false;
+var gotTotal = false;
+var gotOperator = false;
+var operator = "";
+var display = document.getElementById("display");
+display.textContent = "";
+
+// clear the display
+varClear = document.getElementById("clear");
+varClear.addEventListener('click', displayClear, false);
+
+// build the number buttons
+for (i = 1; i <= 9; i++){
+    varKey = document.getElementById("key" + i);
+    varKey.addEventListener('click', makeNum, false);
+}
+
+// build the operator buttons
+varAdd = document.getElementById("key10");
+varAdd.addEventListener('click', getOperator, false);
+varSub = document.getElementById("key11");
+varSub.addEventListener('click', getOperator, false);
+varMult = document.getElementById("key12");
+varMult.addEventListener('click', getOperator, false);
+varDiv = document.getElementById("key13");
+varDiv.addEventListener('click', getOperator, false);
+
+// evaluate the equation
+varProcess = document.getElementById("process");
+varProcess.addEventListener('click', getVal, false);
